ae0313c877778752069c851c0af405693137530f
Oscar Vergara <oscar.vergara1994@gmail.com>
2023-02-14 06:00:50 -0500

    Added homepage component to app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
 
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./components/pages/HomePage";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
