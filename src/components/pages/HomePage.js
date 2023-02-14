2d590e7035eba331261c22bad2b412ffb3cb4a12
Oscar Vergara <oscar.vergara1994@gmail.com>
2023-02-14 06:01:38 -0500

    Added Authentication form to Homepage

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,5 +1,14 @@
 import React from "react";
+import AuthenticationForm from "../AuthenticationForm";
 
 export default function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      HomePage
+      <div>
+        <AuthenticationForm />
+        Dolore nostrud exercitation cillum aliqua.
+      </div>
+    </div>
+  );
 }
