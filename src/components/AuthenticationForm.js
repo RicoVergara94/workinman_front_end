a59e900dd024d35134ae4633865c6dcd67987931
Oscar Vergara <oscar.vergara1994@gmail.com>
2023-02-14 06:46:53 -0500

    fixed issue with event defaulting

--- src/components/AuthenticationForm.js
+++ src/components/AuthenticationForm.js
@@ -2,12 +2,23 @@ import React, { useState } from "react";
 
 export default function AuthenticationForm() {
   const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-  const handleSubmit = () => {};
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const res = await fetch("http://localhost:3232/");
+    const data = await res.json();
+    console.log(data);
+  };
+  console.log("here");
+
+  //   fetch("http://localhost:3232")
+  //     .then((res) => res.json())
+  //     .then((data) => console.log(data));
 
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input placeholder="username" />
         <input placeholder="password" />
         <button type="submit">Submit</button>
