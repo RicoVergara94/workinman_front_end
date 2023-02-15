2ef4383dcc88e17cdc786085794900c3b392d370
Oscar Vergara <oscar.vergara1994@gmail.com>
2023-02-15 14:19:57 -0500

    Added functionality ot registration page

--- src/components/pages/RegistrationPage.js
+++ src/components/pages/RegistrationPage.js
@@ -1,5 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function RegistrationPage() {
-  return <div>Registration Page</div>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleUsernameChange = ({ target }) => {
+    const { name, value } = target;
+    setUsername(value);
+  };
+  const handlePasswordChange = ({ target }) => {
+    const { name, value } = target;
+    setPassword(value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const res = await fetch("http://localhost:3232/register", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    });
+  };
+
+  return (
+    <div>
+      <h1>Registration Page</h1>
+      <form onSubmit={handleSubmit}>
+        <div>Please enter your username</div>
+        <input
+          placeholder="username"
+          name="username"
+          value={username}
+          onChange={handleUsernameChange}
+        />
+        <div>Please enter your password</div>
+        <input
+          placeholder="password"
+          name="password"
+          value={password}
+          onChange={handlePasswordChange}
+        />
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
+  );
 }
