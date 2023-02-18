6cf6ab0c2a4a23397fe672a8fd5140060c457c5e
Oscar Vergara <oscar.vergara1994@gmail.com>
2023-02-18 17:03:18 -0500

    Added a path to the delete component page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import HomePage from "./components/pages/HomePage";
 import RegistrationPage from "./components/pages/RegistrationPage";
+import DeleteAccountPage from "./components/pages/DeleteAccountPage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/register" element={<RegistrationPage />} />
+          <Route path="/delete" element={<DeleteAccountPage />} />
         </Routes>
       </Router>
     </>
