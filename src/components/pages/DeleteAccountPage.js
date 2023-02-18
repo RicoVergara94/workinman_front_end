3c0ff5b5f7c91b0271d6808cd9bd8cc8221df33a
Oscar Vergara <oscar.vergara1994@gmail.com>
2023-02-18 15:07:44 -0500

    Added delete Account page

---
+++ src/components/pages/DeleteAccountPage.js
@@ -0,0 +1,6 @@
+import React, { useState } from "react";
+
+export default function DeleteAccountPage() {
+  const [state, setstate] = useState(initialState);
+  return <div>DeleteAccountPage</div>;
+}
