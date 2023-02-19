ce85df8bda4fb96a52db5b5afb50ae0d7b5dc3b4
Oscar Vergara <oscar.vergara1994@gmail.com>
2023-02-19 11:31:24 -0500

    Created Successful account was deleted component

---
+++ src/components/AccountWasDeleted.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function AccountWasDeleted() {
+  return (
+    <>
+      <div className="success-message">
+        <p>Your account has been successfully deleted.</p>
+      </div>
+    </>
+  );
+}
