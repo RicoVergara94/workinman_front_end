804bd071fd5ee646344b413897b22d7d016c0a8e
Oscar Vergara <oscar.vergara1994@gmail.com>
2023-02-21 18:15:45 -0500

    Added imageformcomponent to userprofile page

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,5 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import ImageFormComponent from "./ImageFormComponent";
 // Need props with the username and other things
 export default function UserProfile(props) {
-  return <div>Welcome {props.username}!</div>;
+  //   const [component, setComponent] = useState();
+
+  const [selectedImages, setSelectedImages] = useState([]);
+
+  return (
+    <>
+      <header>Welcome {props.username}!</header>
+      <main>
+        <div>
+          <ImageFormComponent />
+        </div>
+        {/* {component} */}
+      </main>
+    </>
+  );
 }
